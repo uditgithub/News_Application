117654cf722daea9c2254b58eb9c667f2dba9829
uditgithub <udit14066@gmail.com>
2024-08-01 12:38:22 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,96 @@
+const darkModeToggle = document.getElementById("darkModeToggle");
+const body = document.body;
+document.getElementById('footerText').innterHTML = `Copyright &copy;${new Date().getFullYear()} &nbsp; Team Adwitiya`
+// Function to toggle dark mode based on user preference
+function toggleDarkMode() {
+  if (darkModeToggle.checked) {
+    body.classList.add("dark-mode");
+  } else {
+    body.classList.remove("dark-mode");
+  }
+}
+
+darkModeToggle.addEventListener("change", toggleDarkMode);
+
+function setInitialDarkMode() {
+  const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+  if (prefersDarkMode) {
+    body.classList.add("dark-mode");
+    darkModeToggle.checked = true;
+  }
+}
+
+let newsBox = document.getElementById("newsBox");
+let spinner = document.getElementById("spinner");
+let newsCategory = [
+  "national",
+  "business",
+  "sports",
+  "world",
+  "politics",
+  "technology",
+  "startup",
+  "entertainment",
+  "miscellaneous",
+  "hatke",
+  "science",
+  "automobile",
+];
+
+const xhr = new XMLHttpRequest();
+
+function sendCategory(index) {
+  getNews(newsCategory[index]);
+}
+getNews("all");
+
+function getNews(newsCategoryName) {
+  xhr.open(
+    "GET",
+    `https://inshortsapi.vercel.app/news?category=${newsCategoryName}`,
+    true
+  );
+
+  xhr.getResponseHeader("Content-type", "application/json");
+
+  xhr.onload = function () {
+    if (this.status === 200) {
+      let json = JSON.parse(this.responseText);
+      let data = json.data;
+
+      let newsHTML = "";
+
+      function showSpinner() {
+        spinner.style.visibility = "hidden";
+        newsBox.style.visibility = "visible";
+      }
+
+      xhr.onprogress = showSpinner;
+
+      for (key in data) {
+        let news = `<div class="newsCard">
+        <div class="imageWrapper">
+        <img src="${data[key].imageUrl}"
+        class="thumnail" alt="Image">
+            </div>
+            <div class="card-body">
+            <div class="card-date">${data[key].date}</div>
+                      <h5 class="card-title">${data[key].title}</h5>
+                                <h5 class="card-author">Author: ${data[key].author}</h5>
+                                <p class="card-text">${data[key].content}</p>
+                                <a target="_blank" href="${data[key].readMoreUrl}" class="btn btn-primary">Read more..</a>
+                            </div>
+                           
+                        </div>`;
+        newsHTML += news;
+      }
+
+      newsBox.innerHTML = newsHTML;
+    } else {
+      console.log("Some Error Occurred");
+    }
+  };
+
+  xhr.send();
+}
